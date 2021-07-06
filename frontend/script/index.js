065a59daceab86e7ac8690704647daec37c11223
faturar <64860344+FaturRAR@users.noreply.github.com>
2021-07-06 14:59:13 +0700

    Creating Artikel Pages and Make Responsive
    Creating artikel page and responsive a homepage

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -24,3 +24,10 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+(function() { // DON'T EDIT BELOW THIS LINE
+  var d = document, s = d.createElement('script');
+  s.src = 'https://smkn-1-kemang-1.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
