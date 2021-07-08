6a5438a737e4dcca80759145adbf7120cd6c3da4
faturar <64860344+FaturRAR@users.noreply.github.com>
2021-07-08 11:18:39 +0700

    change to 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
+const {DateTime} = require('luxon')
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/library');
     eleventyConfig.addPassthroughCopy('./src/script');
     eleventyConfig.addPassthroughCopy('./src/styles');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    });
 
     return {
         dir: {
