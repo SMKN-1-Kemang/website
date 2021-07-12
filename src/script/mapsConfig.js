9c4b8f5b5502185f857d6e4e6dfebb1d62f6cb9e
faturar <64860344+FaturRAR@users.noreply.github.com>
2021-07-12 20:30:09 +0700

    fix alll

--- src/script/mapsConfig.js
+++ src/script/mapsConfig.js
@@ -1,16 +1,16 @@
 // Maps Config
 function initMap() {
-    const myLatLng = { lat: -6.500693, lng: 106.747215 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: myLatLng,
-    });
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "SMKN 1 Kemang",
-    });
-  }
+  const myLatLng = { lat: -6.500693, lng: 106.747215 };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: myLatLng,
+  });
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "SMKN 1 Kemang",
+  });
+}
   
-  initMap();
-  // End Maps Config
+initMap();
+// End Maps Config
