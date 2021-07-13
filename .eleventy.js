6abfeea48bd38a707d541f87d940dad820588855
faturar <64860344+FaturRAR@users.noreply.github.com>
2021-07-13 12:51:46 +0700

    Refactoring code

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,12 @@
 const {DateTime} = require('luxon');
-const searchFilter = require("./src/script/searchFilter");
 
 module.exports = function(eleventyConfig) {
+    // Make all static
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/library');
     eleventyConfig.addPassthroughCopy('./src/script');
     eleventyConfig.addPassthroughCopy('./src/styles');
     eleventyConfig.addPassthroughCopy('./src/admin');
-
-    eleventyConfig.addFilter("search", searchFilter);
-
-    eleventyConfig.addCollection("news", collection => {
-        return [...collection.getFilteredByGlob("./src/news/**/*.md")];
-    });
 
     eleventyConfig.addFilter("dateFilter", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
