7b71986821ac1c6261ee034876aac54ed356ace3
faturar <64860344+FaturRAR@users.noreply.github.com>
2021-07-26 15:55:19 +0700

    Fix slider kompetensi keahlian

--- src/script/splideConfig.js
+++ src/script/splideConfig.js
@@ -1,9 +1,15 @@
 // Spilde Config
 new Splide( '.splide', {
+	type: 'loop',
+	focus: 'center',
 	perPage: 3,
+	padding: {
+		right: '5rem',
+		left: '5rem',
+	},
 	breakpoints: {
-		'640': {
-			perPage: 2,
+		'768': {
+			perPage: 1,
 			gap    : '1rem',
 		},
 		'480': {
